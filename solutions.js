a5921285440c75047c7d58cb8d97c40d4462e473
Your Name <tpunchihewage@gmail.com>
2022-07-13 14:36:46 +0200

    added solutions.js

---
+++ solutions.js
@@ -0,0 +1,38 @@
+// Exercise - 01
+
+function multiplyNum(a, b) {
+  return a * b;
+}
+
+console.log(multiplyNum(4, 2));
+
+// Exercise - 02
+
+let storeValue = function (a, b) {
+  return a * b;
+};
+console.log(storeValue(5, 2));
+
+// Exercise - 03
+
+let arrowValues = (a, b) => a * b;
+console.log(arrowValues(2, 2));
+
+// Exercise - 04
+
+function multiplyNum(a, b) {
+  return a / b;
+}
+
+console.log(multiplyNum(4, 2));
+// -------------------------------------
+
+let storeValue2 = function (a, b) {
+  return a / b;
+};
+console.log(storeValue2(5, 2));
+
+// -------------------------------------
+
+let arrowValues2 = (a, b) => a / b;
+console.log(arrowValues2(2, 2));
